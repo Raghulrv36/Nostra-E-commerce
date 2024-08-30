a1c40c844dd17ad53bcefc92625ebdf0a19dd6b2
Raghulrv36 <raghulvenkatachalam23@gmail.com>
2024-08-30 14:56:49 +0530

    contact page corrections

---
+++ contact.js
@@ -0,0 +1,7 @@
+// offer bar
+var offerBar = document.getElementById("offer-bar")
+
+document.getElementById("offer-close").addEventListener("click",function(){
+    offerBar.style.display="none"
+}
+)
